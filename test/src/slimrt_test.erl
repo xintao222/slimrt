cf908d0e1c92c4616d5bc53716e23108d3194e04
Ery Lee <ery.lee@gmail.com>
2014-11-21 11:41:01 +0800

    add test

---
+++ test/src/slimrt_test.erl
@@ -0,0 +1,68 @@
+-module(slimrt_test).
+
+-export([start/1]).
+
+-define(DOMAIN, "localhost").
+-define(APIKEY, "public").
+-define(SERVER, "http://localhost:8080/v1").
+
+start([Max]) ->
+    inets:start(permanent),
+    Num = list_to_integer(atom_to_list(Max)), 
+    run(Num, Num).
+
+run(0, _Max) ->
+	done;
+
+run(I, Max) ->
+    Name = integer_to_list(I),
+    Nick = lists:concat(["user", Name]),
+    Buddies = [integer_to_list(random:uniform(Max)) || _I <- lists:seq(1, 20)],
+    Rooms = [integer_to_list(random:uniform(Max)) || _I <- lists:seq(1, 10)],
+    Params = [
+        {"domain", ?DOMAIN}, 
+        {"name", Name}, 
+        {"nick", Nick}, 
+        {"buddies", string:join(Buddies, ",")}, 
+        {"rooms", string:join(Rooms, ",")}
+    ],
+    Body = string:join([lists:concat([K, "=", V]) || {K, V} <- Params], "&"),
+    Request = {?SERVER ++ "/presences/online", headers(), "application/x-www-form-urlencoded", Body},
+    io:format("~p~n", [Request]),
+    Profile = profile(I),
+    inets:start(httpc, [{profile, Profile}, {timeout, 120000}]),
+    case httpc:request(post, Request, [{timeout, 30000}], [], Profile) of
+    {ok, {_Status, _Headers, Resp}} ->
+        {struct, JSON} = mochijson2:decode(Resp),
+        io:format("start loop ~p ~n", [I]),
+        spawn(fun() -> loop(JSON, Profile) end);
+    {error, Reason} ->
+        io:format("Error: ~p~n", [Reason])
+    end,
+    timer:sleep(400),
+    run(I-1, Max).
+
+loop(JSON, Profile) ->
+    Server = binary_to_list(proplists:get_value(<<"jsonpd">>, JSON)),
+    Ticket = binary_to_list(proplists:get_value(<<"ticket">>, JSON)),
+    Params = [{"domain", ?DOMAIN}, {"ticket", Ticket}],
+    Query = string:join([lists:concat([K, "=", V]) || {K, V} <- Params], "&"),
+    Url = Server ++ "?" ++ Query,
+    io:format("poll: ~s~n", [Ticket]),
+    case httpc:request(Url, Profile) of
+    {error, Reason} ->
+        io:format("~s poll error: ~p~n", [Profile, Reason]);
+    {ok, {_Status, _Headers, Body}} ->
+        io:format("~s loop response: ~p~n", [Profile, Body]),
+        timer:sleep(5),
+        loop(JSON, Profile)
+    end.
+
+profile(I) ->
+    list_to_atom("httpc_" ++ integer_to_list(I)).
+
+headers() ->
+    Auth = base64:encode_to_string(?DOMAIN ++ ":" ++ ?APIKEY), 
+    [{"Authorization", "Basic " ++  Auth}].
+    
+
